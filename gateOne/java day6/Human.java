a4783f5acb35eead70c933b71069eb47e3672c2f
obanla69 <obanlayinka2323@gmail.com>
2024-05-12 20:23:17 +0100

    add files

---
+++ gateOne/java day6/Human.java
@@ -0,0 +1,49 @@
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Scanner;
+
+
+	public class Human{
+         
+
+
+              public LocalDate dateOfBirth;
+
+		public static void main(String[]args){
+
+		Scanner input = new Scanner(System.in);
+			
+		   System.out.print("Enter date of birth in DD-MM-YY : ");
+			
+			 String user = input.nextLine();
+
+
+			   LocalDate dob= LocalDate.parse(user);
+
+			     System.out.println("You are " + user + " years old ");
+				
+
+		}
+		
+			public static int calculateAge(String user){
+
+		                          int age = years - year;
+		
+		                    if (days != user || curDate !=  null){
+			                   age = yearUserYear - 1;
+						}
+		
+				else{
+
+				age =  year - userYear;
+				System.out.print(" you are {age}years old");
+
+			
+		}
+
+	}
+
+}
+
+	                                                                                       
---
+++ gateOne/python day6/taskSix.py
@@ -0,0 +1,21 @@
+import datetime
+
+def getYears(birth_date, today):
+
+	BIRTH_DAY = datetime.datetime().("2022-12-11")
+
+	TODAY = datetime.datetime.now()
+
+	YEAR = 60 * 60 * 24 * 365
+
+	OLD = (today - birth_date) / year
+
+	return OLD
+
+
+	Age = getYears("2022-12-11" , datetime.datetime.now())
+
+print(Age)
+	
+
+	
