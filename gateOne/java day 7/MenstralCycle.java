82c115bcc4fe6a7a6db3d235b8ea926ed201e92d
obanla69 <obanlayinka2323@gmail.com>
2024-05-13 15:02:24 +0100

    add files

---
+++ gateOne/java day 7/MenstralCycle.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+  import java.time.Period;
+   import java.time.LocalDate;
+
+
+	public class MenstralCycle{
+
+           public static void main(String[]args){
+
+	       String menstralCycle = """
+		To calculate cycle;
+		press
+	
+		1-> flow date
+		2-> safe periods
+		3->  ovaluation period
+		""";		
+	
+		System.out.print(menstralCycle);
+		Scanner input = new Scanner(System.in);
+		int cycle = input.nextInt();
+
+		switch(cycle){
+		case 1 :
+		System.out.println("Flow date");
+		case 2 :
+		System.out.println("safe period");
+		case 3 :
+		System.out.println("ovaluation period");
+		
+		}
+
+		
+	
+
+
+
+	}
+
+
+}
