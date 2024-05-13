8da6f67afebd31e2964483149abdfcf1a4d309f9
obanla69 <obanlayinka2323@gmail.com>
2024-05-13 17:00:14 +0100

    add files

--- gateOne/java day 7/MenstralCycle.java
+++ gateOne/java day 7/MenstralCycle.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
   import java.time.Period;
    import java.time.LocalDate;
+   import java.util.Date;
 
 
 	public class MenstralCycle{
@@ -27,6 +28,34 @@ import java.util.Scanner;
 		System.out.println("safe period");
 		case 3 :
 		System.out.println("ovaluation period");
+
+
+		String flowDate = """
+
+		to calculate the flow date
+		""";
+		
+		
+
+		System.out.print("Enter a current date of your menstral pain : ");
+		int curDate = input.nextInt();
+
+			int month = 30;	
+
+		LocalDate startDate = LocalDate.now().minusMonths(1);
+		LocalDate endDate =  LocalDate.now();
+		Period fromMonths = Period.ofMonths(1);
+		Date date = new Date();
+		
+		curDate = curDate - month;
+		
+		System.out.printf("CurDate between startDate and: " %d%n + " endDate is :  %d%n + ", Period.ofMonths(1));
+
+
+
+
+
+
 		
 		}
 
