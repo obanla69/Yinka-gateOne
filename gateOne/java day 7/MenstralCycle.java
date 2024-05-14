debed0c9e1db64926a84c5be674c3c2f4f4df3f1
obanla69 <obanlayinka2323@gmail.com>
2024-05-14 15:12:47 +0100

    add files

--- gateOne/java day 7/MenstralCycle.java
+++ gateOne/java day 7/MenstralCycle.java
@@ -36,26 +36,34 @@ import java.util.Scanner;
 		to calculate the flow date
 		""";
 
-		System.out.print("what is the last month of your period : (14-05-2024)");
+		System.out.println("what is the last month of your period : (14/05/2024)");
 		String lastMonth = input.nextLine();
 
-		System.out.print("How many days is your period " );
+		System.out.println("How many days is your period " );
 		int days = input.nextInt();
+		
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");			System.out.println("Todays date is " + today);		
+		LocalDate freeFlow = today.plusMonths(1);
+		System.out.println("Your free period is " + freeFlow);
 
-
-		LocalDate date = LocalDate.of(2024,14,5);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		String formattedDate = date.format(formatter);	
-
-		LocalDate newDate = date.plusDays(5);
-		System.out.print(formattedDate);
-
-
+		int sum = 0;
+		int averageLengthCycle = 0;
+			
+		System.out.print("Enter a number : ");
+		int number1 = input.nextInt();
+		int number2 = input.nextInt();
+		int number3 = input.nextInt();
+	 	 sum = number1 + number2 + number3;
+		averageLengthCycle /= sum;
+		System.out.printf("AverageLengthCycle is %.2f%n", + averageLengthCycle);
+			
+	
+				
 
 		}
 
 
 	}
-
 
 }
